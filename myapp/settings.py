efffe7bf9203d4b4f0cc1efb72526b1283f91b97
Aamir Ansari <sabriaamir74@gmail.com>
2021-02-13 11:33:47 +0530

    added req procfile

--- myapp/settings.py
+++ myapp/settings.py
@@ -130,6 +130,5 @@ STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets') 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 MEDIA_URL='/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media/') 
